478e5413cf79abca5f3cf37fad611cca40ad32d0
Phoenix Z Sun <phoenixzsun@outlook.com>
2018-04-24 17:38:57 +0800

    安卓打release包图表不展示、ios显示比例太小

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -3,22 +3,13 @@ import { WebView, View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
+const source = (Platform.OS == 'ios') ? require('./tpl.html') : {'uri':'file:///android_asset/echarts/tpl.html'}
+
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.setNewOption = this.setNewOption.bind(this);
-  }
-  
-
   componentWillReceiveProps(nextProps) {
     if(nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
-  }
-
-  setNewOption(option) {
-    this.refs.chart.postMessage(JSON.stringify(option));
   }
 
   render() {
@@ -28,12 +19,12 @@ export default class App extends Component {
           ref="chart"
           scrollEnabled = {false}
           injectedJavaScript = {renderChart(this.props)}
+          scalesPageToFit={Platform.OS === 'android' ? true : false}
           style={{
             height: this.props.height || 400,
-            backgroundColor: this.props.backgroundColor || 'transparent'
           }}
-          scalesPageToFit={Platform.OS !== 'ios'}
-          source={require('./tpl.html')}
+          source={source}
+          style={{backgroundColor: this.props.option.backgroundColor || 'rgba(0, 0, 0, 0)'}}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
